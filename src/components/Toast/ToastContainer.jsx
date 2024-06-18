704d30f421d0dfa1e8d4ddc300a94b10dd1520ed
Jieun Oh <ohjieun@kakao.com>
2024-06-19 01:02:52 +0900

    Feat: toast 정적 UI 구현 완료
    - tailwind theme 추가
    - fontFamily Pretendard 추가
    - toast 컨테이너의 높이를 위한 maxHeight 추가(screen-margin-24)
    - tailwind에 없는 width 추가
    - 스크롤 CSS 변경
    - 토스트 메시지 mock data 추가

--- src/components/Toast/ToastContainer.jsx
+++ src/components/Toast/ToastContainer.jsx
@@ -1,5 +1,23 @@
+import ToastMessage from "./ToastMessage";
+import TextButton from "../shared/Button/TextButton";
+
+import messageList from "../../utils/message.json";
+
 function ToastContainer() {
-  return <div></div>;
+  return (
+    <aside className="fixed p-3 overflow-x-hidden overflow-y-auto max-h-screen-margin-24 bottom-5 right-5 w-88">
+      <div className="h-6">
+        <TextButton onClick={() => {}}>
+          <span className="text-slate-700">모두 지우기</span>
+        </TextButton>
+      </div>
+      <ul className="flex flex-col-reverse flex-wrap-reverse items-center flex-nowrap ">
+        {messageList.map(({ id, message, link }) => (
+          <ToastMessage key={id} message={message} link={link} />
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 export default ToastContainer;

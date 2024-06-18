1fb770ced9bc2655d29c3f163deb2ba9b14e153b
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-06-19 02:24:37 +0900

    Feat: url textarea 컴포넌트 구현
    - url 입력시 행 갯수에 따라 늘어나는 textarea 구현
    - 3줄 초과시 높이 고정하는 기능 추가

--- src/components/Header/UrlInputContainer.jsx
+++ src/components/Header/UrlInputContainer.jsx
@@ -1,5 +1,29 @@
+import { useRef } from "react";
+
 function UrlInputContainer() {
-  return <div>url container</div>;
+  const textarea = useRef();
+
+  const handleResizeHeight = () => {
+    textarea.current.style.height = "auto";
+
+    const computedStyle = getComputedStyle(textarea.current);
+    const lineHeight = parseInt(computedStyle.lineHeight, 10);
+    const maxHeight = lineHeight * 3;
+    const newHeight = Math.min(textarea.current.scrollHeight, maxHeight);
+
+    textarea.current.style.height = `${newHeight}px`;
+  };
+
+  return (
+    <div className="border-[1px] border-light-gray flex items-center justify-center m-auto mx-auto bg-white shadow-md w-fit rounded-xl">
+      <textarea
+        ref={textarea}
+        rows={1}
+        onChange={handleResizeHeight}
+        className="m-3 mx-5 outline-none w-[35rem] font-thin text-base resize-none"
+      />
+    </div>
+  );
 }
 
 export default UrlInputContainer;

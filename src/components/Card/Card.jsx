663e4cc6b089402968036ac9936f5b14920dcedd
Pangho04 <dlckdgh741@naver.com>
2024-06-18 23:53:05 +0900

    Feat: CardContainer, Card 컴포넌트 추가
    - Mock 데이터를 받아와 필요 정보 표시
    - 'CardContainer' 컴포넌트 내에 Card 컴포넌트 그리드 레이아웃 표시.

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,43 @@
+import { FiMinus } from "react-icons/fi";
+import PropTypes from "prop-types";
+
+import ReadingTime from "./ReadingTime";
+import Button from "../shared/Button/Button";
+
+function Card({ favicon, domain, articleTitle, readingTime }) {
+  return (
+    <li className="relative content-center w-40 h-40 pl-5 list-none transition-all delay-75 bg-white shadow-md group hover:scale-125 shadow-black/25 rounded-3xl">
+      <a
+        href="https://www.google.com"
+        target="_blank"
+        className="relative -top-1"
+        rel="noreferrer"
+      >
+        <div>
+          <img
+            className="inline-block w-4"
+            src={favicon || "public/favicon.png"}
+            alt="favicon"
+          />
+          <p className="text-[11px] inline-block ml-1 font-medium">{domain}</p>
+        </div>
+        <ReadingTime readingTime={readingTime} />
+        <p className="mt-1 mr-5 group-hover:pointed-title font-extralight title-ellipsis">
+          {articleTitle}
+        </p>
+      </a>
+      <Button>
+        <FiMinus color="gray" strokeWidth={4} />
+      </Button>
+    </li>
+  );
+}
+
+export default Card;
+
+Card.propTypes = {
+  favicon: PropTypes.string.isRequired,
+  domain: PropTypes.string.isRequired,
+  articleTitle: PropTypes.string.isRequired,
+  readingTime: PropTypes.number.isRequired,
+};
--- src/components/Card/CardContainer.jsx
+++ src/components/Card/CardContainer.jsx
@@ -1,5 +1,20 @@
+import Card from "./Card";
+import articles from "../../utils/URLs.json";
+
 function CardContainer() {
-  return <main></main>;
+  return (
+    <main className="grid min-w-[40rem] justify-center grid-cols-10 gap-6 bg-fixed bg-right-bottom bg-no-repeat justify-items-center bg-default mx-80">
+      {articles.map((article) => (
+        <Card
+          key={article.createDate}
+          favicon={article.faviconUrl}
+          domain={article.siteName}
+          articleTitle={article.title}
+          readingTime={article.readingTime}
+        ></Card>
+      ))}
+    </main>
+  );
 }
 
 export default CardContainer;

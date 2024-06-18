e83de53c1de34799bc0ca618b17829f65dbb09fb
Jieun Oh <ohjieun@kakao.com>
2024-06-19 01:52:27 +0900

    Design: 버튼 hover 배경색 추가, 공백 제거

--- src/components/shared/Button/IconButton.jsx
+++ src/components/shared/Button/IconButton.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 function IconButton({ children, onClick, title }) {
   return (
     <button
-      className="absolute flex items-center justify-center px-2 text-xs text-center rounded-full -top-3 -right-3 h-7 min-w-4 bg-light-gray drop-shadow"
+      className="absolute flex items-center justify-center px-2 text-xs text-center rounded-full -top-3 -right-3 h-7 min-w-4 bg-light-gray drop-shadow hover:bg-medium-gray"
       type="button"
       onClick={onClick}
       title={title}
--- src/components/shared/Button/TextButton.jsx
+++ src/components/shared/Button/TextButton.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 function TextButton({ children, onClick }) {
   return (
     <button
-      className="absolute top-0 right-0 flex items-center justify-center px-2 text-xs text-center rounded-full h-7 min-w-4 bg-light-gray drop-shadow"
+      className="absolute top-0 right-0 flex items-center justify-center px-2 text-xs text-center rounded-full h-7 min-w-4 bg-light-gray drop-shadow hover:bg-medium-gray"
       type="button"
       onClick={onClick}
     >

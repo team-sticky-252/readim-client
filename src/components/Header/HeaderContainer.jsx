ef62b795b04099db6fc87a0645d6e0f5d91becb5
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-06-18 16:58:42 +0900

    Layout: headerContainer 구성 컴포넌트 생성

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,5 +1,15 @@
+import InfoTextContainer from "./InfoTextContainer";
+import OptionContainer from "./OptionContainer";
+import UrlInputContainer from "./UrlInputContainer";
+
 function HeaderContainer() {
-  return <header></header>;
+  return (
+    <header>
+      <OptionContainer />
+      <InfoTextContainer />
+      <UrlInputContainer />
+    </header>
+  );
 }
 
 export default HeaderContainer;
---
+++ src/components/Header/UrlInputContainer.jsx
@@ -0,0 +1,5 @@
+function UrlInputContainer() {
+  return <div></div>;
+}
+
+export default UrlInputContainer;

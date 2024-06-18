3b36087f4e2194e8e4563f6760e923dd5bfd3fb5
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-06-19 03:17:42 +0900

    Refactor: 옵션 컴포넌트 내 팝업 버튼 로직 경량화

--- src/components/Header/OptionPopUpButton.jsx
+++ src/components/Header/OptionPopUpButton.jsx
@@ -1,36 +1,21 @@
 import PropTypes from "prop-types";
 
 function OptionPopUpButton({ text, position }) {
+  let borderRadius = "";
+  if (position === "top") {
+    borderRadius = "rounded-t-lg";
+  } else if (position === "bottom") {
+    borderRadius = "rounded-b-lg";
+  }
+
   return (
-    <>
-      {position === "top" && (
-        <button
-          type="button"
-          className="font-extralight h-8 bg-white rounded-t-lg hover:bg-[#E5E5E5]"
-          onClick={() => {}}
-        >
-          {text}
-        </button>
-      )}
-      {position === "bottom" && (
-        <button
-          type="button"
-          className="font-extralight h-8 bg-white rounded-b-lg hover:bg-[#E5E5E5]"
-          onClick={() => {}}
-        >
-          {text}
-        </button>
-      )}
-      {position === "middle" && (
-        <button
-          type="button"
-          className="font-extralight h-8 bg-white hover:bg-[#E5E5E5]"
-          onClick={() => {}}
-        >
-          {text}
-        </button>
-      )}
-    </>
+    <button
+      type="button"
+      className={`font-extralight h-8 bg-white ${borderRadius} hover:bg-[#E5E5E5]`}
+      onClick={() => {}}
+    >
+      {text}
+    </button>
   );
 }
 

bb9e4bf2996f5811c931bf4f8c3c3ff5209447f1
Jieun Oh <ohjieun@kakao.com>
2024-06-19 00:58:54 +0900

    Layout: 버튼 컴포넌트 분리
    - 기존 버튼 컴포넌트를 children prop을 기준으로 분리했습니다.
    - children이 아이콘인 경우 IconButton
    - children이 텍스트를 가진 요소인 경우 TextButton

---
+++ src/components/shared/Button/IconButton.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+function IconButton({ children, onClick, title }) {
+  return (
+    <button
+      className="absolute flex items-center justify-center px-2 text-xs text-center rounded-full -top-3 -right-3 h-7 min-w-4 bg-light-gray drop-shadow"
+      type="button"
+      onClick={onClick}
+      title={title}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default IconButton;
+
+IconButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+};
---
+++ src/components/shared/Button/TextButton.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+function TextButton({ children, onClick }) {
+  return (
+    <button
+      className="absolute top-0 right-0 flex items-center justify-center px-2 text-xs text-center rounded-full h-7 min-w-4 bg-light-gray drop-shadow"
+      type="button"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default TextButton;
+
+TextButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func.isRequired,
+};

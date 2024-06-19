55348cff80a90c351baca25e8ad8e83958096d30
Pangho04 <dlckdgh741@naver.com>
2024-06-19 21:16:16 +0900

    -Feat: 상황별 모달창 정적 구현
    - 각 상활별 모달창 구성 구현.
    - 모달창 별 버튼 네비게이트 구현으로 다음 상황 모달창으로 연계.

--- src/components/Modal/ModalWrapper.jsx
+++ src/components/Modal/ModalWrapper.jsx
@@ -1,5 +1,14 @@
+import Modals from "./Modals";
+
 function ModalWrapper() {
-  return <div></div>;
+  return (
+    <>
+      <div className="fixed top-0 left-0 ixed w-full h-full bg-[#ECECEC] opacity-60" />
+      <div className="modal-position">
+        <Modals />
+      </div>
+    </>
+  );
 }
 
 export default ModalWrapper;

463b2eaa482ec86f3192772a229e9b547508cc2b
Pangho04 <dlckdgh741@naver.com>
2024-06-19 22:08:14 +0900

    Style: CSS단위, import문 순서 수정

--- src/components/Modal/ModalWrapper.jsx
+++ src/components/Modal/ModalWrapper.jsx
@@ -3,7 +3,7 @@ import Modals from "./Modals";
 function ModalWrapper() {
   return (
     <>
-      <div className="fixed top-0 left-0 ixed w-full h-full bg-[#ECECEC] opacity-60" />
+      <div className="fixed top-0 left-0 w-full h-full bg-[#ECECEC] opacity-60" />
       <div className="modal-position">
         <Modals />
       </div>

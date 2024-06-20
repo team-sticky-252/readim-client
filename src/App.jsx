9e26610a84aac71f801360e437f4eb7a75de530f
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-06-20 21:22:15 +0900

    Feat: 첫 방문 시 메인 페이지에 환영 메세지 모달 띄우는 로직 작성
    - 메인 페이지 렌더링 시 로컬 스토리지를 확인해 읽기 속도인 wpm을 확인합니다.
    - wpm이 존재하지 않는 경우 기본값인 202를 저장합니다.
    - 이후 모달 컴포넌트를 렌더링합니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+
+import { Outlet, useNavigate } from "react-router-dom";
 
 import HeaderContainer from "./components/Header/HeaderContainer";
 import CardContainer from "./components/Card/CardContainer";
 import ToastContainer from "./components/Toast/ToastContainer";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!window.localStorage.getItem("wpm")) {
+      window.localStorage.setItem("wpm", "202");
+      navigate("/modal/welcome");
+    }
+  }, [window.localStorage.getItem("wpm")]);
+
   return (
     <>
       <HeaderContainer />

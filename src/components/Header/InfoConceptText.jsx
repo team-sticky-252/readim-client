d9cfff0c1dd6fbddb1a3335531996efc7f37f4cd
Pangho04 <dlckdgh741@naver.com>
2024-06-22 16:35:11 +0900

    Design: ConceptTextBox CSS 수정

--- src/components/Header/InfoConceptText.jsx
+++ src/components/Header/InfoConceptText.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const CONCEPT = ["아낄", "볼", "활용할", "계획할"];
+const CONCEPT = ["아낄 ", "파악할", "활용할", "계획할"];
 
 function InfoConceptText() {
   const [index, setIndex] = useState(0);
@@ -20,7 +20,7 @@ function InfoConceptText() {
 
   return (
     <span
-      className={`inline-block w-14 text-right text-[#54CABC] ${animate ? "ease-linear duration-300 opacity-0 -translate-y-3" : "ease-linear duration-300 opacity-100"}`}
+      className={`whitespace-pre inline-block w-14 text-right text-[#54CABC] ${animate ? "ease-linear duration-300 opacity-0 -translate-y-3" : "ease-linear duration-300 opacity-100"}`}
     >
       {CONCEPT[index]}
     </span>

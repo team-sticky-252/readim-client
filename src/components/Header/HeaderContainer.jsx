246c48f0856ca72d0bfc7506751c50b64b110044
Pangho04 <dlckdgh741@naver.com>
2024-06-22 21:25:13 +0900

    Feat: 총 ReadTime 안내 기능
    - 로컬 스토리지 상의 정보 업데이트시, 총 리드 타임 갱신 후 표시.
    - 총 리드 타임 0일 시, 컨셉 텍스트 표시.

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,17 +1,46 @@
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
 import InfoTextContainer from "./InfoTextContainer";
 import OptionContainer from "./OptionContainer";
 import UrlInputContainer from "./UrlInputContainer";
 
-function HeaderContainer() {
-  const totalReadTime = 690000;
+function HeaderContainer({
+  prevArticleDatas,
+  setPrevArticleDatas,
+  setMessageList,
+  setIsDeleteMode,
+}) {
+  const [totalReadTime, setTotalReadTime] = useState(0);
+
+  useEffect(() => {
+    if (prevArticleDatas && prevArticleDatas.length > 0) {
+      let readTime = 0;
+      prevArticleDatas.forEach((articleData) => {
+        readTime += Number(articleData?.data?.readingTime);
+      });
+      setTotalReadTime(readTime);
+    }
+  }, [prevArticleDatas]);
 
   return (
     <header className="grid w-full text-center text-centerborder-solid grid-row-3">
-      <OptionContainer />
+      <OptionContainer setIsDeleteMode={setIsDeleteMode} />
       <InfoTextContainer totalReadTime={totalReadTime} />
-      <UrlInputContainer />
+      <UrlInputContainer
+        prevArticleDatas={prevArticleDatas}
+        setPrevArticleDatas={setPrevArticleDatas}
+        setMessageList={setMessageList}
+      />
     </header>
   );
 }
 
 export default HeaderContainer;
+
+HeaderContainer.propTypes = {
+  prevArticleDatas: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  setPrevArticleDatas: PropTypes.func.isRequired,
+  setMessageList: PropTypes.func.isRequired,
+  setIsDeleteMode: PropTypes.func.isRequired,
+};

db112f7f538ac1949869f99ca549abde08c7676a
Pangho04 <dlckdgh741@naver.com>
2024-06-22 21:29:35 +0900

    Feat: URL 입력 창 동적 UI 구현
    - URL 최대 30개 까지 입력 가능.
    - 엔터 버튼 키다운or 붙여넣기 시 request 요청.
    - response 유효성 검사 후, 로컬 스토리지 저장.
    - 잘못된 입력값, , 중복값, response 상대값 등 오류별 toast 메시지 표시.

--- src/components/Header/UrlInputContainer.jsx
+++ src/components/Header/UrlInputContainer.jsx
@@ -1,17 +1,87 @@
 import { useRef } from "react";
+import PropTypes from "prop-types";
 
-function UrlInputContainer() {
+import message from "../../utils/message.json";
+import {
+  requestURL,
+  errorCase,
+  isValid,
+  handleResizeHeight,
+} from "../../utils/urlUtils";
+
+function UrlInputContainer({
+  prevArticleDatas,
+  setPrevArticleDatas,
+  setMessageList,
+}) {
   const textarea = useRef();
 
-  const handleResizeHeight = () => {
-    textarea.current.style.height = "auto";
+  const handleURLInput = async (event) => {
+    let pasteValue = "";
 
-    const computedStyle = getComputedStyle(textarea.current);
-    const lineHeight = parseInt(computedStyle.lineHeight, 10);
-    const maxHeight = lineHeight * 3;
-    const newHeight = Math.min(textarea.current.scrollHeight, maxHeight);
+    if (event.clipboardData) {
+      pasteValue = event.clipboardData.getData("text");
+    } else if (window.clipboardData) {
+      pasteValue = window.clipboardData.getData("text");
+    }
 
-    textarea.current.style.height = `${newHeight}px`;
+    const inputValue = pasteValue || textarea.current.value;
+    const trimmedInputValue = inputValue.trim();
+    const urls = trimmedInputValue.match(/(https?:\/\/[^\s]+)/g) || [];
+    const otherValues = trimmedInputValue
+      .split(/\s+/)
+      .filter((value) => !value.match(/(https?:\/\/[^\s]+)/g));
+    const inputValues = [...urls, ...otherValues];
+
+    if (event.keyCode === 13 || pasteValue) {
+      const handleSingleURL = async (url) => {
+        const articleDatas = await requestURL(url);
+        const { statusCode } = articleDatas;
+        const isValidResult = isValid(
+          url,
+          prevArticleDatas,
+          setMessageList,
+          textarea,
+          message,
+          articleDatas.data ? articleDatas.data.url : null,
+        );
+
+        if (!isValidResult) {
+          return null;
+        }
+
+        if (statusCode !== 200) {
+          setMessageList((prev) => [
+            ...prev,
+            {
+              id: prev ? prev.length + 1 : 1,
+              message: errorCase(statusCode, message),
+              link: url || null,
+            },
+          ]);
+          return null;
+        }
+
+        articleDatas.createDate = new Date(
+          new Date().toLocaleString("en-US", { timeZone: "Asia/Seoul" }),
+        ).toISOString();
+
+        return articleDatas;
+      };
+
+      const promises = inputValues.map(handleSingleURL);
+      const results = await Promise.all(promises);
+      const validResults = results.filter((result) => result !== null);
+      const updatedArticleDatas = prevArticleDatas
+        ? [...prevArticleDatas, ...validResults]
+        : validResults;
+
+      window.localStorage.setItem("URLs", JSON.stringify(updatedArticleDatas));
+      setPrevArticleDatas(updatedArticleDatas);
+
+      textarea.current.value = "";
+      textarea.current.style.height = "auto";
+    }
   };
 
   return (
@@ -19,11 +89,27 @@ function UrlInputContainer() {
       <textarea
         ref={textarea}
         rows={1}
-        onChange={handleResizeHeight}
+        onChange={() => handleResizeHeight(textarea)}
+        onKeyDown={handleURLInput}
+        onPaste={handleURLInput}
         className="m-3 mx-5 outline-none w-[35rem] font-thin text-base resize-none"
       />
     </div>
   );
 }
+
+UrlInputContainer.propTypes = {
+  prevArticleDatas: PropTypes.arrayOf(
+    PropTypes.shape({
+      data: PropTypes.shape({
+        url: PropTypes.string.isRequired,
+      }).isRequired,
+      createDate: PropTypes.string,
+      statusCode: PropTypes.number,
+    }),
+  ).isRequired,
+  setPrevArticleDatas: PropTypes.func.isRequired,
+  setMessageList: PropTypes.func.isRequired,
+};
 
 export default UrlInputContainer;
--- src/components/Toast/ToastContainer.jsx
+++ src/components/Toast/ToastContainer.jsx
@@ -1,17 +1,19 @@
+import PropTypes from "prop-types";
+
 import ToastMessage from "./ToastMessage";
 import TextButton from "../shared/Button/TextButton";
 
-import messageList from "../../utils/message.json";
-
-function ToastContainer() {
+function ToastContainer({ messageList }) {
   return (
     <aside className="fixed p-3 overflow-x-hidden overflow-y-auto max-h-screen-margin-24 bottom-5 right-5 w-88">
-      <div className="h-6">
-        <TextButton onClick={() => {}}>
-          <span className="text-slate-700">모두 지우기</span>
-        </TextButton>
-      </div>
-      <ul className="flex flex-col-reverse flex-wrap-reverse items-center flex-nowrap">
+      {messageList.length !== 0 && (
+        <div className="h-6">
+          <TextButton onClick={() => {}}>
+            <span className="text-slate-700">모두 지우기</span>
+          </TextButton>
+        </div>
+      )}
+      <ul className="flex flex-col-reverse items-center whitespace-pre-wrap flex-nowrap">
         {messageList.map(({ id, message, link }) => (
           <ToastMessage key={id} message={message} link={link} />
         ))}
@@ -21,3 +23,13 @@ function ToastContainer() {
 }
 
 export default ToastContainer;
+
+ToastContainer.propTypes = {
+  messageList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      message: PropTypes.string.isRequired,
+      link: PropTypes.string,
+    }),
+  ).isRequired,
+};

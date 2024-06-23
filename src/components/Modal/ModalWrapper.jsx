fcb2bb956fec479d881a9a014152ccd88c99b1c1
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-06-23 17:17:26 +0900

    Fix: 읽기 시간 측정 로직 오차 수정
    - 컴포넌트 내 변수로 선언하여 계산할 때 발생하는 측정 시간 오차를 state를 사용해 최소화 하였습니다.

--- src/components/Modal/ModalWrapper.jsx
+++ src/components/Modal/ModalWrapper.jsx
@@ -1,11 +1,19 @@
-import Modals from "./Modal";
+import { useOutletContext } from "react-router-dom";
+import Modal from "./Modal";
 
 function ModalWrapper() {
+  const [firstClickTimeMs, setFirstClickTimeMs] = useOutletContext();
+  const [clickTimeDifferenceMs, setClickTimeDifferenceMs] = useOutletContext();
+
   return (
     <>
       <div className="fixed top-0 left-0 w-full h-full bg-[#ECECEC] opacity-60" />
       <div className="modal-position">
-        <Modals />
+        <Modal
+          firstClickTimeMs={firstClickTimeMs}
+          setFirstClickTimeMs={setFirstClickTimeMs}
+          setClickTimeDifferenceMs={setClickTimeDifferenceMs}
+        />
       </div>
     </>
   );

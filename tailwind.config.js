174c988884e82641f191495a6b4e3e1b415c26af
Pangho04 <dlckdgh741@naver.com>
2024-06-23 11:54:40 +0900

    Design: 토스트 메세지 애니메이션 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,16 @@ export default {
       width: {
         88: "22rem",
       },
+      keyframes: {
+        "slide-top": {
+          "0%": { transform: "translateY(100px)" },
+          "100%": { transform: "translateY(0px)" },
+        },
+      },
+      animation: {
+        "slide-top":
+          "slide-top 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],

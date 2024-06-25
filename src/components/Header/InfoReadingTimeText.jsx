032b5b72676c1c2f6192fa9e99a131bf3d68b89f
Jieun Oh <ohjieun@kakao.com>
2024-06-26 02:33:33 +0900

    Feat: InfoReadingTimeText 60분 이상일 경우 시간 단위 문구 추가

--- src/components/Header/InfoReadingTimeText.jsx
+++ src/components/Header/InfoReadingTimeText.jsx
@@ -2,19 +2,20 @@ import PropTypes from "prop-types";
 
 function InfoReadingTimeText({ totalReadTime }) {
   let readingMinute = Math.floor(totalReadTime / 1000 / 60);
-  let readingSeconds =
+  const readingSeconds =
     Math.round((totalReadTime / 1000 - readingMinute * 60) * 0.1) * 10;
 
   if (readingSeconds >= 30) {
     readingMinute += 1;
-    readingSeconds = 0;
-  } else {
-    readingSeconds = 0;
   }
+
+  const readingHour = Math.floor(readingMinute / 60);
+  readingMinute -= readingHour * 60;
 
   return (
     <span className="inline-block font-medium text-right text-primary">
-      {`${readingMinute}분`}
+      {readingHour !== 0 && `${readingHour}시간 `}
+      {readingMinute !== 0 && `${readingMinute}분`}
     </span>
   );
 }

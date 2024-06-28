de0f358093e7ae06ea2af69f7d6b6818c88fea63
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-06-28 19:02:31 +0900

    Design: 옵션버튼 z-index 추가
    - 헤더 그라데이션 추가로 인해 옵션 버튼이 겹치게 되어 z-index를 추가했습니다.

--- src/components/shared/Button/TextButton.jsx
+++ src/components/shared/Button/TextButton.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 function TextButton({ children, onClick = () => {} }) {
   return (
     <button
-      className="absolute flex items-center justify-center px-2 text-xs text-center rounded-full -right-1.5 -top-2 h-7 min-w-4 bg-light-gray drop-shadow hover:bg-medium-gray"
+      className="absolute flex items-center z-10 justify-center px-2 text-xs text-center rounded-full -right-1.5 -top-2 h-7 min-w-4 bg-light-gray drop-shadow hover:bg-medium-gray"
       type="button"
       onClick={onClick}
     >

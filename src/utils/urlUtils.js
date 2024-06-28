b92d86f012c7ed4738c8a330362c47b02b6a90f5
Pangho04 <dlckdgh741@naver.com>
2024-06-29 03:24:25 +0900

    Fix: 호스트 주소 프록시 수정

--- src/utils/urlUtils.js
+++ src/utils/urlUtils.js
@@ -3,7 +3,7 @@ import ERROR_MESSAGE from "./errorMessage";
 const BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:8080"
-    : "http://readimtest03-env.eba-5ebns2mz.ap-northeast-2.elasticbeanstalk.com";
+    : "https://readimtest03-env.eba-5ebns2mz.ap-northeast-2.elasticbeanstalk.com";
 
 export const requestURL = async (inputValue, option = {}) => {
   const requestParam = {

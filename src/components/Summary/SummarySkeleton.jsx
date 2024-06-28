674c499bd5d51da171b382e1c49c7bc1d4e0c451
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-06-28 19:00:45 +0900

    Layout: 스켈레톤 컴포넌트 추가

---
+++ src/components/Summary/SummarySkeleton.jsx
@@ -0,0 +1,11 @@
+function SummarySkeleton() {
+  return (
+    <div className="space-y-4 animate-pulse">
+      <div className="w-3/4 h-5 opacity-50 rounded-xl bg-gray"></div>
+      <div className="w-1/2 h-5 opacity-50 rounded-xl bg-gray"></div>
+      <div className="w-full h-5 opacity-50 rounded-xl bg-gray"></div>
+    </div>
+  );
+}
+
+export default SummarySkeleton;

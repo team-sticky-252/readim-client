edce463b30475f173b1fe113893cdf06b8ece5bb
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-06-29 04:20:19 +0900

    Design: 옵션 버튼 위치 수정

--- src/components/Header/OptionButton.jsx
+++ src/components/Header/OptionButton.jsx
@@ -8,7 +8,7 @@ function OptionButton() {
   return (
     <div className="real">
       <button
-        className="fixed z-10 p-1 cursor-pointer peer top-5 right-5 hover:bg-medium-gray hover:rounded-lg"
+        className="fixed z-10 p-1 cursor-pointer group peer top-5 right-5 hover:bg-medium-gray hover:rounded-lg"
         aria-label="open options"
         type="button"
         onClick={() => navigate("/modal/guide")}
@@ -18,12 +18,12 @@ function OptionButton() {
           size={22}
           filter="drop-shadow(1px 1px 2px rgb(0 0 0 / 0.25))"
         />
+        <span className="absolute hidden w-40 top-0.5 -right-4 group-hover:block">
+          <TextButton>
+            <span className="m-2 text-slate-700">읽기 속도 재측정</span>
+          </TextButton>
+        </span>
       </button>
-      <span className="absolute hidden min-w-[50%] right-16 top-7 peer-hover:inline-block">
-        <TextButton>
-          <span className="m-2 text-slate-700">읽기 속도 재측정</span>
-        </TextButton>
-      </span>
     </div>
   );
 }

b22f79b557009c853d7a9da09957b594ede9b51e
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-06-28 20:22:33 +0900

    Design: 읽기 속도 재설정 버튼 설명 위치 변경
    - 기존 css 구조 변경으로 인해 읽기속도 재설정 버튼에 hover 시 확인할 수 있는 설명의 위치를 재조정하였습니다.

--- src/components/Header/OptionButton.jsx
+++ src/components/Header/OptionButton.jsx
@@ -1,21 +1,30 @@
 import { IoSettingsSharp } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 
+import TextButton from "../shared/Button/TextButton";
+
 function OptionButton() {
   const navigate = useNavigate();
   return (
-    <button
-      className="fixed p-1 cursor-pointer peer top-5 right-5 hover:bg-medium-gray hover:rounded-lg"
-      aria-label="open options"
-      type="button"
-      onClick={() => navigate("/modal/guide")}
-    >
-      <IoSettingsSharp
-        color="white"
-        size={22}
-        filter="drop-shadow(1px 1px 2px rgb(0 0 0 / 0.25))"
-      />
-    </button>
+    <div className="real">
+      <button
+        className="fixed z-10 p-1 cursor-pointer peer top-5 right-5 hover:bg-medium-gray hover:rounded-lg"
+        aria-label="open options"
+        type="button"
+        onClick={() => navigate("/modal/guide")}
+      >
+        <IoSettingsSharp
+          color="white"
+          size={22}
+          filter="drop-shadow(1px 1px 2px rgb(0 0 0 / 0.25))"
+        />
+      </button>
+      <span className="absolute hidden min-w-[50%] right-16 top-7 peer-hover:inline-block">
+        <TextButton>
+          <span className="m-2 text-slate-700">읽기 속도 재측정</span>
+        </TextButton>
+      </span>
+    </div>
   );
 }
 
--- src/components/Header/OptionContainer.jsx
+++ src/components/Header/OptionContainer.jsx
@@ -1,15 +1,9 @@
-import TextButton from "../shared/Button/TextButton";
 import OptionButton from "./OptionButton";
 
 function OptionContainer() {
   return (
-    <div className="flex justify-end h-20">
+    <div className="relative flex justify-end h-20">
       <OptionButton />
-      <span className="absolute hidden min-w-[50%] right-16 top-7 peer-hover:inline-block">
-        <TextButton>
-          <span className="m-2 text-slate-700">읽기 속도 재측정</span>
-        </TextButton>
-      </span>
     </div>
   );
 }

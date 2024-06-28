bb95e080e609438f51b2323b4b35a246ba72476e
Jieun Oh <ohjieun@kakao.com>
2024-06-28 21:57:56 +0900

    Chore: Cypress 기본 설정

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   test: {
-    coverage: {
-      provider: "v8",
-      reporter: ["text"],
-      reportsDirectory: "./src/spec/coverage",
-      exclude: ["**/*config*.*"],
-    },
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/spec/setupTest.js",
-    testMatch: ["./src/spec/**/*.spec.jsx"],
+    setupFiles: "./src/setupTests.js",
+    testMatch: ["./src/spec/*.spec.jsx"],
   },
 });

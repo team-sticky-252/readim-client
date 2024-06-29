f4fb0097473639cc22c8db5b2f56a10cbab3285f
Pangho04 <dlckdgh741@naver.com>
2024-06-29 20:23:03 +0900

    Design: 리액트 아이콘 gradient 효과 컴포넌트 추가

---
+++ src/components/shared/GradientPatchCheckIcon.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { BsPatchCheckFill } from "react-icons/bs";
+
+function GradientPatchCheckIcon() {
+  return (
+    <div>
+      <svg width="0" height="0">
+        <defs>
+          <linearGradient
+            id="gradient1"
+            gradientTransform="rotate(22)"
+            x1="0%"
+            y1="0%"
+            x2="100%"
+            y2="70%"
+          >
+            <stop
+              offset="0%"
+              style={{ stopColor: "#8ac0a7", stopOpacity: 1 }}
+            />
+            <stop
+              offset="100%"
+              style={{ stopColor: "#9ad9f2", stopOpacity: 1 }}
+            />
+          </linearGradient>
+        </defs>
+      </svg>
+      <BsPatchCheckFill
+        style={{ fill: "url(#gradient1)", width: "1rem", height: "1.5rem" }}
+      />
+    </div>
+  );
+}
+
+export default GradientPatchCheckIcon;

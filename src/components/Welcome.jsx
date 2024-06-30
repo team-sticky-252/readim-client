22415b84ec08166a8972b6a9e4430ce82ca58bba
Jieun Oh <ohjieun@kakao.com>
2024-06-30 14:58:44 +0900

    Design: 초기화면 CSS 수정
    - 폰트 색상 진하게 변경
    - 불필요하게 사용된 CSS 제거

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,12 +3,11 @@ import InfoConceptText from "./Header/InfoConceptText";
 function Welcome() {
   return (
     <div className="flex justify-center mt-48">
-      <div className="w-3/5 font-thin text-7xl">
-        <p className="inline-block py-2 text-neutral-300">
-          <span className="font-medium">Readim</span>
-          <span>은 시간을</span>
+      <div className="w-3/5 font-thin text-7xl text-zinc-600">
+        <p className="py-2">
+          <span>Readim은 시간을</span>
         </p>
-        <p className="inline-block py-2 text-neutral-300">
+        <p className="py-2">
           <InfoConceptText />
           <span>&nbsp;수 있어요.</span>
         </p>

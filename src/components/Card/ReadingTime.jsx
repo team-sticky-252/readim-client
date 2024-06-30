cfb660e28d6b57187df48fff16aa38894a2c6ddb
Jieun Oh <ohjieun@kakao.com>
2024-07-01 01:16:21 +0900

    Refactor: reading time 초 단위 반올림 로직 변경
    - 카드 내에 보이는 아티클의 예상 읽기 시간은 초 단위 반올림 없이 그대로 화면에 노출
    - 총 읽기 시간은 올림으로 변경

--- src/components/Card/ReadingTime.jsx
+++ src/components/Card/ReadingTime.jsx
@@ -1,18 +1,8 @@
 import PropTypes from "prop-types";
 
 function ReadingTime({ readingTime }) {
-  let readingMinute = Math.floor(readingTime / 1000 / 60);
-  let readingSeconds =
-    Math.round((readingTime / 1000 - readingMinute * 60) * 0.1) * 10;
-
-  if (readingSeconds >= 45) {
-    readingMinute += 1;
-    readingSeconds = 0;
-  } else if (readingSeconds >= 15) {
-    readingSeconds = 30;
-  } else {
-    readingSeconds = 0;
-  }
+  const readingMinute = Math.floor(readingTime / 1000 / 60);
+  const readingSeconds = Math.floor(readingTime / 1000 - readingMinute * 60);
 
   return (
     <div className="my-3">
--- src/components/Header/InfoReadingTimeText.jsx
+++ src/components/Header/InfoReadingTimeText.jsx
@@ -2,10 +2,9 @@ import PropTypes from "prop-types";
 
 function InfoReadingTimeText({ totalReadTime }) {
   let readingMinute = Math.floor(totalReadTime / 1000 / 60);
-  const readingSeconds =
-    Math.round((totalReadTime / 1000 - readingMinute * 60) * 0.1) * 10;
+  const readingSeconds = Math.floor(totalReadTime / 1000 - readingMinute * 60);
 
-  if (readingSeconds >= 30) {
+  if (readingSeconds > 0) {
     readingMinute += 1;
   }
 

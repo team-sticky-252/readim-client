580602c6e2560c37a452b7932e702cbd5a5a4152
Jieun Oh <ohjieun@kakao.com>
2024-06-30 15:27:05 +0900

    Design: Welcome 페이지 Readim 문구 Font Weight 변경

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,11 +5,11 @@ function Welcome() {
     <div className="flex justify-center mt-48">
       <div className="w-3/5 font-thin text-7xl text-zinc-600">
         <p className="py-2">
-          <span>Readim은 시간을</span>
+          <span className="font-medium">Readim</span>은 시간을
         </p>
         <p className="py-2">
           <InfoConceptText />
-          <span>&nbsp;수 있어요.</span>
+          &nbsp;수 있어요.
         </p>
       </div>
     </div>

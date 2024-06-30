7b4ff0c7da10e32e5643bbe65874ba78d83457d8
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-07-01 05:40:46 +0900

    Style: 잘못된 클래스명 수정

--- src/components/Summary/SummaryMessage.jsx
+++ src/components/Summary/SummaryMessage.jsx
@@ -1,6 +1,6 @@
 function SummaryMessage({ children }) {
   return (
-    <div className="px-5 py-3 mt-2 font-normal text-white break-all ext-base max-w-96 rounded-xl bg-pan-left bg-gradient-to-r from-pastel-purple via-blush-pink to-sunset-orange">
+    <div className="px-5 py-3 mt-2 text-base font-normal text-white break-all max-w-96 rounded-xl bg-pan-left bg-gradient-to-r from-pastel-purple via-blush-pink to-sunset-orange">
       {children}
     </div>
   );

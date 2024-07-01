0b6e74c453b663f8a3cab61c79d95f17176ee821
Pangho04 <dlckdgh741@naver.com>
2024-07-02 00:21:50 +0900

    Design: position 속성별 안내 팝업 위치 대응(fixed)
    - postion별 특성에 맞춰 CSS 설정을 추가했습니다.
    - 불필요해진 요소를 삭제했습니다.

--- src/components/Header/OptionButton.jsx
+++ src/components/Header/OptionButton.jsx
@@ -1,16 +1,15 @@
 import { IoSettingsSharp } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 
-import TextButton from "../shared/Button/TextButton";
-
 function OptionButton() {
   const navigate = useNavigate();
   return (
     <div className="real">
       <button
-        className="fixed z-10 p-1 cursor-pointer group peer top-5 right-5 hover:bg-medium-gray hover:rounded-lg"
+        className="fixed z-10 p-1 cursor-pointer hovertext group peer top-5 right-5 hover:bg-medium-gray hover:rounded-lg"
         aria-label="open options"
         type="button"
+        data-hover="읽기 속도 재측정"
         onClick={() => navigate("/modal/guide")}
       >
         <IoSettingsSharp
@@ -18,11 +17,6 @@ function OptionButton() {
           size={22}
           filter="drop-shadow(1px 1px 2px rgb(0 0 0 / 0.25))"
         />
-        <span className="absolute w-max hidden top-0.5 right-9 group-hover:block">
-          <TextButton>
-            <span className="m-2 text-slate-700">읽기 속도 재측정</span>
-          </TextButton>
-        </span>
       </button>
     </div>
   );

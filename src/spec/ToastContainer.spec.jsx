e2b695d5a7d7d8803c8c6efd3ec4dab488ddb42b
Pangho04 <dlckdgh741@naver.com>
2024-07-05 23:37:16 +0900

    Test: ToastContainer 컴포넌트 테스트 코드 작성

---
+++ src/spec/ToastContainer.spec.jsx
@@ -0,0 +1,70 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+
+import ToastContainer from "../components/Toast/ToastContainer";
+import { act } from "react";
+
+describe("Test ToastMessage Component", () => {
+  it("Toast Mesaage should show its props's texts", () => {
+    render(
+      <ToastContainer
+        messageList={[
+          {
+            id: "1",
+            icon: "✅",
+            messages: ["passed the test"],
+            link: "have test",
+          },
+        ]}
+        deleteMessage={() => {}}
+        deleteAllMessages={() => {}}
+      />,
+    );
+
+    const iconElement = screen.getByText("✅");
+    const messageElement = screen.getByText("passed the test");
+    const linkElement = screen.getByText("have test");
+
+    expect(iconElement).toBeInTheDocument();
+    expect(messageElement).toBeInTheDocument();
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("When delete Allmessages, Should play fade-out-animation during 0.5s and completly empty the messages", () => {
+    const deleteAllMessagesMock = vi.fn();
+
+    const { container } = render(
+      <ToastContainer
+        messageList={[
+          {
+            id: "1",
+            icon: "✅",
+            messages: ["passed the test"],
+            link: "have test",
+          },
+        ]}
+        deleteMessage={() => {}}
+        deleteAllMessages={deleteAllMessagesMock}
+      />,
+    );
+
+    const deleteAllbutton = screen.getByText("모두 지우기").parentElement;
+
+    vi.useFakeTimers();
+
+    act(() => {
+      fireEvent.click(deleteAllbutton);
+    });
+
+    expect(container.firstChild).toHaveClass("animate-fade-out-bottom");
+
+    act(() => {
+      vi.advanceTimersByTime(1500);
+    });
+
+    expect(deleteAllMessagesMock).toHaveBeenCalled();
+
+    const hasNoPendingTimers = vi.getTimerCount() === 0;
+    expect(hasNoPendingTimers).toBe(true);
+  });
+});

3f05af149407c0043d0f98ec0395777d9fecb614
Jieun Oh <ohjieun@kakao.com>
2024-08-12 00:13:37 +0900

    Design: 헤더 관련 컴포넌트 모바일 대응
    - 컨셉 문구 폰트 크기 축소
    - URL 입력창 길이 변경

--- src/components/Header/InfoTextContainer.jsx
+++ src/components/Header/InfoTextContainer.jsx
@@ -5,7 +5,7 @@ import InfoReadingTimeContainer from "./InfoReadingTimeContainer";
 function InfoTextContainer({ totalReadTime }) {
   return (
     <div
-      className={`flex relative bottom-6 justify-center ${totalReadTime > 0 && "h-28"}`}
+      className={`flex relative bottom-6 justify-center ${totalReadTime > 0 && "h-28 max-mobile:h-8"}`}
     >
       {totalReadTime > 0 && (
         <InfoReadingTimeContainer totalReadTime={totalReadTime} />
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,7 +3,7 @@ import InfoConceptText from "./Header/InfoConceptText";
 function Welcome() {
   return (
     <div className="flex justify-center mt-16">
-      <div className="w-3/5 font-thin text-7xl text-zinc-600">
+      <div className="w-3/5 font-thin text-7xl text-zinc-600 max-mobile:w-auto max-mobile:text-4xl">
         <p className="py-2">
           <span className="font-medium">Readim</span>은 시간을
         </p>

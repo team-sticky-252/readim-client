7352168e5db9c87ca2e86f658f307c7e727440cd
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-08-12 22:26:33 +0900

    Design: 테스트 결과 페이지 레이아웃 변경

--- src/components/Service/ResultService.jsx
+++ src/components/Service/ResultService.jsx
@@ -46,7 +46,7 @@ function ResultService({ clickTimeDifferenceMs, navigateNextPage }) {
       isDisabledButton={false}
     >
       <div className="flex flex-col items-center font-light text-center">
-        <div className="mb-4 text-2xl whitespace-pre-line mt-14">
+        <div className="mb-4 text-2xl whitespace-pre-line mt-14 max-mobile:text-lg max-mobile:mt-0">
           <p className="py-4">
             ⏱️ {ARTICLE_WORD_COUNT}개의 단어를 읽는데{" "}
             <span className="relative font-medium z-1 highlight">
@@ -56,7 +56,7 @@ function ResultService({ clickTimeDifferenceMs, navigateNextPage }) {
           </p>
         </div>
         <Pie size={200} value={wpm} />
-        <div className="mb-4 text-lg whitespace-pre-line mt-14">
+        <div className="mb-4 text-lg whitespace-pre-line mt-14 max-mobile:text-base max-mobile:mt-6">
           <p className="mb-1">{resultMessage}</p>
           <p>이 속도에 맞춰서 예상 읽기 시간을 제공해 드릴게요.</p>
           <p className="mt-6 text-sm text-neutral-400">

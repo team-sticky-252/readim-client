00c95296fbd84a98a51df0af8adc211500eef84c
Lee Jaehoon <99021245+dlgnswk@users.noreply.github.com>
2024-08-26 21:48:47 +0900

    Refactor: default WPM에 맞게 word count를 202로 변경

--- src/utils/readTimeTest.js
+++ src/utils/readTimeTest.js
@@ -1,5 +1,5 @@
 export const DEFAULT_WPM = 202;
-export const ARTICLE_WORD_COUNT = 203;
+export const ARTICLE_WORD_COUNT = 202;
 export const MIN_READING_TIME_MS = 25000;
 export const MAX_READING_TIME_MS = 145000;
 
